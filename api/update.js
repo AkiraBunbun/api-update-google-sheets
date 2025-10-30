541974a9c8f3c02a36139248d34fa05f662932a2
YourLovelyGirl <104588336+AkiraBunbun@users.noreply.github.com>
2025-10-30 19:04:56 +0100

    Refactor import statements and update export syntax and switching from ECMAScript to NodeJS

--- api/update.js
+++ api/update.js
@@ -1,18 +1,14 @@
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-import { GoogleAuth } from 'google-auth-library';
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { GoogleAuth } = require('google-auth-library');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (replace '*' with your frontend domain for tighter security)
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow specific HTTP methods
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
 
-  // Log to check if headers are set correctly
-  console.log('CORS headers set');
-
   // Handle preflight OPTIONS request
   if (req.method === 'OPTIONS') {
-    console.log('OPTIONS request received');
     return res.status(200).end(); // Respond successfully to preflight request
   }
 
@@ -54,7 +50,6 @@ export default async function handler(req, res) {
       updated: valores
     });
   } catch (error) {
-    console.error('Error:', error.message);
     res.status(500).json({ error: error.message });
   }
-}
+};
